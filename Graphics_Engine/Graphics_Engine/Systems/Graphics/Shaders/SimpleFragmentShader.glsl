fea53fa5e0ee6e4c55f78199fb15bafc2cf07d61
Bilbobull <bilbobul@hotmail.com>
2015-09-29 00:58:35 -0700

    Shader changes

--- Graphics_Engine/Graphics_Engine/Systems/Graphics/Shaders/SimpleFragmentShader.glsl
+++ Graphics_Engine/Graphics_Engine/Systems/Graphics/Shaders/SimpleFragmentShader.glsl
@@ -1,9 +1,10 @@
 #version 330
 
 in vec4 Color;
+in vec3 Normal;
 out vec4 outColor;
 
 void main()
 {
-	outColor = Color;
+	outColor = vec4 (Normal, 1.0);
 }
--- Graphics_Engine/Graphics_Engine/Systems/Graphics/Shaders/SimpleVertexShader.glsl
+++ Graphics_Engine/Graphics_Engine/Systems/Graphics/Shaders/SimpleVertexShader.glsl
@@ -1,11 +1,13 @@
 #version 330
 
 layout (location = 0) in vec3 position;
-layout (location = 1) in vec3 vertexColor;
+layout (location = 1) in vec3 normal;
+layout (location = 2) in vec3 vertexColor;
 //layout (location = 2) in mat4 FullTransformMatrix;
 
 uniform mat4 FullTransformMatrix;
  
+out vec3 Normal;
 out vec4 Color;
 
 void main ()
@@ -13,5 +15,6 @@ void main ()
   vec4 v = vec4(position, 1.0);
 
   gl_Position = FullTransformMatrix * v;
+  Normal = normal;
   Color = vec4(vertexColor.rbg, 0.4);
 }
